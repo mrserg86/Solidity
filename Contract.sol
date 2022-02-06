bc3d2329548ee643939f62583c623999c52e0bd0
mrserg86 <84500437+mrserg86@users.noreply.github.com>
2022-02-06 13:58:06 +0300

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -53,27 +53,16 @@ contract Mrserg86TokenCoin is Ownable {
         return false;
     }
     
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
-        if( allowed[_from][msg.sender] >= _value &&
-            balances[_from] >= _value 
-            && balances[_to] + _value >= balances[_to]) {
-            allowed[_from][msg.sender] -= _value;
-            balances[_from] -= _value; 
-            balances[_to] += _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } 
+    function transferFrom() public returns (uint null) {
+        return 0;
+    }
+    
+    function approve() public returns (bool success) {
         return false;
     }
     
-    function approve(address _spender, uint _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-    
-    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
-        return allowed[_owner][_spender];
+    function allowance() public constant returns (bool success) {
+        return false;
     }
     
     event Transfer(address indexed _from, address indexed _to, uint _value);
